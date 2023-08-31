89718b946701013c1676806d520c7aae0dc267f9
gabrieljtt <gabriel_turatti@hotmail.com>
2023-08-31 15:04:37 -0300

    rota para componente nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+
+//Componentes do Projeto
 import { NavComponent } from './components/nav/nav.component';
 
 
