64ffe7c7def3512f6d1f75cdd49c138c355f1a4a
gabrieljtt <gabriel_turatti@hotmail.com>
2023-10-08 17:30:57 -0300

    Filtro para Chamado-list

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -12,6 +12,7 @@ import { ChamadoService } from 'src/app/services/chamado.service';
 export class ChamadoListComponent implements OnInit {
 
   ELEMENT_DATA: Chamado[] = []
+  FILTERED_DATA: Chamado[] = []
 
   displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
@@ -37,5 +38,38 @@ export class ChamadoListComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  returnStatus(status: any): string {
+    if(status == '0'){
+      return 'ABERTO'
+    } else if(status == '1'){
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  returnPrioridade(prioridade: any): string {
+    if(prioridade == '0'){
+      return 'BAIXA'
+    } else if(prioridade == '1'){
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+
+  orderByStatus(status: any): void {
+    let list: Chamado[] = [];
+    
+    this.ELEMENT_DATA.forEach(element =>{
+      if(element.status == status){
+        list.push(element);
+      }
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(this.FILTERED_DATA);
+    this.dataSource.paginator = this.paginator;
   }
 }
